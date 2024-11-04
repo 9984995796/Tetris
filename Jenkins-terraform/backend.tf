8e2dacd91e3c706b2d3d322f034d23d70d827797
Kartik mishra <69319462+9984995796@users.noreply.github.com>
2024-11-04 14:22:43 +0530

    Update backend.tf

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "kartik-998905" # Replace with your actual S3 bucket name
+    bucket = "kartik-99890" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
